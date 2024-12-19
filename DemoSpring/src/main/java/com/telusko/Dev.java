8e406ecd17e2a3452ef83a4db719f4331f4fad8d
parvamehta123-creator <mehta.parv@northeastern.edu>
2024-12-19 17:28:56 -0500

    starting with springboot

---
+++ DemoSpring/src/main/java/com/telusko/Dev.java
@@ -0,0 +1,18 @@
+package com.telusko;
+
+
+public class Dev {
+
+//    private Laptop laptop;
+
+    int age = 8;
+
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
+    public void build(){
+        System.out.println();
+        System.out.println("Working on some awesome project");
+//        laptop.compile();
+    }
+}
