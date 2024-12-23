9414db146a37da8d799c549d8f096798f29733bc
parvamehta123-creator <mehta.parv@northeastern.edu>
2024-12-22 19:23:23 -0500

    Getter and setter injection

--- DemoSpring/src/main/java/com/telusko/Dev.java
+++ DemoSpring/src/main/java/com/telusko/Dev.java
@@ -3,16 +3,38 @@ package com.telusko;
 
 public class Dev {
 
-//    private Laptop laptop;
+   private Laptop laptop;
 
-    int age = 8;
+    private int age;
 
     public Dev(){
         System.out.println("Dev Constructor");
     }
+
+    public Dev(Laptop laptop) {
+        this.laptop = laptop;
+        System.out.println("Dev1 Constructor");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
     public void build(){
         System.out.println();
         System.out.println("Working on some awesome project");
-//        laptop.compile();
+     laptop.compile();
     }
 }
